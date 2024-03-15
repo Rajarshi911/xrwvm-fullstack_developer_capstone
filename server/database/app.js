0d41b7725956b901bbeb69c9fe69fb33952d9e30
Rajarshi911 <21051237@kiit.ac.in>
2024-03-15 06:44:58 -0400

    changes made from theia lab environment

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,40 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships' });
+      }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    //Write your code here
+    const state = req.params.state;
+    try {
+        const documents = await Dealerships.find({ state: state });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships by state' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    //Write your code here
+    const id = req.params.id;
+    try {
+        const dealer = await Dealerships.find({ id: id });
+        if (!dealer) {
+            return res.status(404).json({ error: 'Dealer not found' });
+        }
+        res.json(dealer);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealer' });
+    }
 });
 
 //Express route to insert review
